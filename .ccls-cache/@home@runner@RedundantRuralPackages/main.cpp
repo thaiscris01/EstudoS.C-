99765cf9fcc346bfb0052a1b27eed39ce415f5b6
Thais Cristina <121523814+thaiscris01@users.noreply.github.com>
2023-03-23 21:28:22 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@RedundantRuralPackages/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int valor1, valor2, variavel;
+  printf(" Digite o valor 1: ");
+  scanf("%d",&valor1);
+  printf(" Digite o valor 2: ");
+  scanf("%d", &valor2);
+  if (valor1 > valor2)
+  {
+      printf("O valor 1 eh maior que o valor 2:");
+}
+if (variavel = valor1 - valor2)
+{
+     printf("A diferença entre os dois valores sera:%d\n",variavel);
+}
+  system("pause");
+}
